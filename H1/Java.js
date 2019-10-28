d809cd41512eb311510816c2bf835ecbea3aaa6f
blaade666 <47110785+blaade666@users.noreply.github.com>
2019-10-28 09:01:32 +0100

    Add files via upload

---
+++ H1/Java.js
@@ -0,0 +1,9 @@
+var Laptop = {
+    merk: "Asus",
+    type: "Gaming",
+    aram: "16 GB",
+    kleur: "zwart"
+}
+
+console.log(Laptop.kleur);
+document.getElementById("Head").innerHTML = Laptop.kleur;
---
+++ H2/oop.js
@@ -0,0 +1,4 @@
+document.getElementById("paginatitel").innerText = "ooop";
+document.getElementById("subkop").innerText = "yuh yuh yuh";
+document.getElementById("pepe").src = "img/pepe.jpg";
+document.getElementById("pepe2").src = "img/pepe2.jpg";
